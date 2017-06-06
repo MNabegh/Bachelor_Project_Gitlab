72fa94b58d47c00172854d6b9020ed3ef6ca328c
Mahmoud Nabegh <mahmoudnabegh@gmail.com>
2017-06-06 17:02:38 +0200

    add all fine dust readings

--- FineDustMeasurementsNew/download.py
+++ FineDustMeasurementsNew/download.py
@@ -4,7 +4,7 @@ import shutil
 days = range(1,32)
 months = range(1,13)
 years = [2016,2017]
-sensors = [1002, 1178, 1098, 2394]
+sensors = [1002, 1178, 1098, 2394,255,481,1973,1286,1462,2630]
 
 for i in years:
 	datey = str(i)
